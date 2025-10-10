0575b62e490b71729916ad509a9ea0689d187b61
vaibhav30-45 <vshrivastava0730@gmail.com>
2025-10-10 17:56:01 +0530

    log in

---
+++ api/signup/signup.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+function signup() {
+    return http.createServer((req, res) => {
+        if (req.url === "/login" && req.method === "GET") {
+            res.writeHead(200, { "Content-Type": "text/plain" });
+            res.end("Welcome to the Login Page!");
+        } else {
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("Page Not Found");
+        }
+    });
+}
+
+module.exports = signup;
---
+++ src/app.js
@@ -0,0 +1,7 @@
+const createServer = require("../api/login/login.js"); 
+const server = createServer();
+
+server.listen(4000, () => {
+    console.log(" Server running at http://localhost:4000");
+});
+
