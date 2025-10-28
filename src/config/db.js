30df159c32ac86205697b4765cb7c5ac7e661b34
Prashant <shrivastavaprashant91@gmail.com>
2025-10-28 10:18:57 +0530

    test changes

--- src/config/db.js
+++ src/config/db.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(' MongoDB connected');
+    console.log(' MongoDB connected...');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
     process.exit(1);
